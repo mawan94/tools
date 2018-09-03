b4120966efd6e77af105189c55c4922544a275a2
mawan123 <17521188586@163.com>
2018-09-03 15:50:10 +0800

    lamda

--- thumbnailator/src/main/java/com/github/mawan94/demo/Demo1.java
+++ thumbnailator/src/main/java/com/github/mawan94/demo/Demo1.java
@@ -3,21 +3,170 @@ package com.github.mawan94.demo;
 import net.coobird.thumbnailator.Thumbnails;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 
 public class Demo1 {
     public static void main(String[] args) throws IOException {
-        //压缩图片文件大小 scale 图片比例缩放（主要因素） outputQuality参数1为最高质量
-        Thumbnails.of(new File("C:\\Users\\PSDZ\\Desktop\\生产订单列表\\1.png")).scale(1f).outputQuality(1f)
-                .toFile(new File("C:\\Users\\PSDZ\\Desktop\\生产订单列表\\11.png"));
+//        //压缩图片文件大小 scale 图片比例缩放（主要因素） outputQuality参数1为最高质量
+//        Thumbnails.of(new File("C:\\Users\\PSDZ\\Desktop\\生产订单列表\\1.png")).scale(1f).outputQuality(1f)
+//                .toFile(new File("C:\\Users\\PSDZ\\Desktop\\生产订单列表\\11.png"));
+//
+//        int i = 10;
+//        System.out.println(i << 1 >> 2);
+//        int j = 9;
+//        System.out.println(j >> 1);
+//
+//        System.out.println(2 << 2);
 
-        int i = 10;
-        System.out.println(i << 1 >> 2);
-        int j = 9;
-        System.out.println(j >> 1);
+        //===============================  lamda  ====================================
 
-        System.out.println(2 << 2);
+        // DEMO 1
+        String[] atp = {"Rafael Nadal", "Novak Djokovic",
+                "Stanislas Wawrinka",
+                "David Ferrer", "Roger Federer",
+                "Andy Murray", "Tomas Berdych",
+                "Juan Martin Del Potro"};
+        List<String> players = Arrays.asList(atp);
+        players.forEach((player) -> System.out.println(player + "; "));
+
+
+        // DEMO 2
+        Runnable r = () -> System.out.println("Hello world !");
+        r.run();
+
+        // DEMO 3
+        new Thread(() -> System.out.println("Hello world !")).start();
+
+        // DEMO 4
+        String[] players1 = {"Rafael Nadal", "Novak Djokovic",
+                "Stanislas Wawrinka", "David Ferrer",
+                "Roger Federer", "Andy Murray",
+                "Tomas Berdych", "Juan Martin Del Potro",
+                "Richard Gasquet", "John Isner"};
+        Arrays.sort(players1, (String s1, String s2) -> (s1.compareTo(s2)));
+        for (int i = 0; i < players1.length; i++) {
+            //System.out.println(players1[i]);
+        }
+
+        // DEMO 5
+        List<Person> javaProgrammers = new ArrayList<Person>() {
+            {
+                add(new Person("Elsdon", "Jaycob", "Java programmer", "male", 43, 2000));
+                add(new Person("Tamsen", "Brittany", "Java programmer", "female", 23, 1500));
+                add(new Person("Floyd", "Donny", "Java programmer", "male", 33, 1800));
+                add(new Person("Sindy", "Jonie", "Java programmer", "female", 32, 1600));
+                add(new Person("Vere", "Hervey", "Java programmer", "male", 22, 1200));
+                add(new Person("Maude", "Jaimie", "Java programmer", "female", 27, 1900));
+                add(new Person("Shawn", "Randall", "Java programmer", "male", 30, 2300));
+                add(new Person("Jayden", "Corrina", "Java programmer", "female", 35, 1700));
+                add(new Person("Palmer", "Dene", "Java programmer", "male", 33, 2000));
+                add(new Person("Addison", "Pam", "Java programmer", "female", 34, 1300));
+            }
+        };
+
+        List<Person> phpProgrammers = new ArrayList<Person>() {
+            {
+                add(new Person("Jarrod", "Pace", "PHP programmer", "male", 34, 1550));
+                add(new Person("Clarette", "Cicely", "PHP programmer", "female", 23, 1200));
+                add(new Person("Victor", "Channing", "PHP programmer", "male", 32, 1600));
+                add(new Person("Tori", "Sheryl", "PHP programmer", "female", 21, 1000));
+                add(new Person("Osborne", "Shad", "PHP programmer", "male", 32, 1100));
+                add(new Person("Rosalind", "Layla", "PHP programmer", "female", 25, 1300));
+                add(new Person("Fraser", "Hewie", "PHP programmer", "male", 36, 1100));
+                add(new Person("Quinn", "Tamara", "PHP programmer", "female", 21, 1000));
+                add(new Person("Alvin", "Lance", "PHP programmer", "male", 38, 1600));
+                add(new Person("Evonne", "Shari", "PHP programmer", "female", 40, 1800));
+            }
+        };
+        // 5.1
+        System.out.println("所有程序员的姓名:");
+        javaProgrammers.forEach((p) -> System.out.printf("%s %s; ", p.getFirstName(), p.getLastName()));
+        System.out.println("");
+        phpProgrammers.forEach((p) -> System.out.printf("%s %s; ", p.getFirstName(), p.getLastName()));
+        System.out.println("");
+
+        // 5.2 增加程序员的工资5
+        System.out.println("给程序员加薪 5% :");
+        Consumer<Person> giveRaise = e -> e.setSalary(e.getSalary() / 100 * 5 + e.getSalary());
+
+        javaProgrammers.forEach(giveRaise);
+        phpProgrammers.forEach(giveRaise);
+
+
+        // 5.3 显示月薪超过1400美元的PHP程序员
+        System.out.println("下面是月薪超过 $1,400 的PHP程序员:");
+        phpProgrammers.stream()
+                .filter((p) -> (p.getSalary() > 1400))
+                .forEach((p) -> System.out.printf("%s %s; ", p.getFirstName(), p.getLastName()));
+        System.out.println("");
+
+        // 5.4 定义过滤器
+        Predicate<Person> ageFilter = (p) -> (p.getAge() > 25);
+        Predicate<Person> salaryFilter = (p) -> (p.getSalary() > 1400);
+        Predicate<Person> genderFilter = (p) -> ("female".equals(p.getGender()));
+
+        System.out.println("下面是年龄大于 24岁且月薪在$1,400以上的女PHP程序员:");
+        phpProgrammers.stream()
+                .filter(ageFilter)
+                .filter(salaryFilter)
+                .filter(genderFilter)
+                .forEach((p) -> System.out.printf("%s %s; ", p.getFirstName(), p.getLastName()));
+
+        // 5.5
+        System.out.println("最前面的3个 Java programmers:");
+        javaProgrammers.stream()
+                .limit(3)
+                .forEach((p) -> System.out.printf("%s %s; ", p.getFirstName(), p.getLastName()));
+        System.out.println("");
+
+        // 5.6 根据名字和薪水排序Java程序员,放到一个list中
+        System.out.println("根据 name 排序,并显示前5个 Java programmers:");
+        List<Person> sortedJavaProgrammers = javaProgrammers
+                .stream()
+                .sorted((p, p2) -> (p.getFirstName().compareTo(p2.getFirstName())))
+                .limit(5)
+                .collect(toList());
+
+        sortedJavaProgrammers.forEach((p) -> System.out.printf("%s %s; %n", p.getFirstName(), p.getLastName()));
+
+        // 5.7
+        System.out.println("工资最低的 Java programmer:");
+        Person pers = javaProgrammers
+                .stream()
+                .min((p1, p2) -> (p1.getSalary() - p2.getSalary()))
+                .get();
+
+        System.out.printf("Name: %s %s; Salary: $%,d.", pers.getFirstName(), pers.getLastName(), pers.getSalary());
+
+        // 5.8
+        System.out.println("将 Java programmers 的 first name 存放到 Set:");
+        Set<String> javaDevFirstName = javaProgrammers
+                .stream()
+                .map(Person::getFirstName)
+                .collect(toSet());
+
+        // 5.9 计算 count, min, max, sum, and average for numbers
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        IntSummaryStatistics stats = numbers
+                .stream()
+                .mapToInt((x) -> x)
+                .summaryStatistics();
+
+        System.out.println("List中最大的数字 : " + stats.getMax());
+        System.out.println("List中最小的数字 : " + stats.getMin());
+        System.out.println("所有数字的总和   : " + stats.getSum());
+        System.out.println("所有数字的平均值 : " + stats.getAverage());
+
+
     }
+
 
 }
